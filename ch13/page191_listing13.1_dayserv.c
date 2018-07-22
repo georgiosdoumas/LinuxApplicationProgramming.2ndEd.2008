44b346ed48a251568939acfe8e4f8cf990d4c55c
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-07-22 13:01:55 +0200

    Create page191_listing13.1_dayserv.c

---
+++ ch13/page191_listing13.1_dayserv.c
@@ -0,0 +1,39 @@
+/* This is saved on a VirtualMachine I have built, that has IP 192.168.122.89
+  and I then opened the firewall for port 13 */
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#define MAX_BUFFER 128
+#define DAYTIME_SERVER_PORT 13
+int main ( void )
+{
+  int serverFd, connectionFd;
+  struct sockaddr_in servaddr;
+  char timebuffer[MAX_BUFFER+1];
+  time_t currentTime;
+  serverFd = socket( AF_INET, SOCK_STREAM, 0 );
+  memset( &servaddr, 0, sizeof(servaddr) );
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(DAYTIME_SERVER_PORT);
+  bind( serverFd,(struct sockaddr *)&servaddr, sizeof(servaddr) );
+  listen( serverFd, 5 );
+  while ( 1 ) 
+  {
+    connectionFd = accept( serverFd,(struct sockaddr *)NULL, NULL );
+    if (connectionFd >= 0 )  /* valid connection created? */
+    {
+       currentTime = time(NULL);
+       snprintf( timebuffer, MAX_BUFFER, "%s\n", ctime(&currentTime) );
+       write( connectionFd, timebuffer, strlen(timebuffer) );
+       close( connectionFd );
+    }
+  }
+}
+
+/* gcc -Wall page191_listing13.1_dayserv.c -o page191_listing13.1_dayserv 
+Run it as a background process so that it will not hold your terminal console : 
+./page191_listing13.1_dayserv & */  
