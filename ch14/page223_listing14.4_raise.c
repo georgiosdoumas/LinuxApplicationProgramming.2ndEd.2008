69fe2dc752c188091ac90160f4884bbbc0a9971d
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-07-23 11:19:29 +0200

    Update page223_listing14.4_raise.c

--- ch14/page223_listing14.4_raise.c
+++ ch14/page223_listing14.4_raise.c
@@ -7,7 +7,7 @@
 
 void usr1_handler( int sig_num )
 {
-  printf( "Parent (%d) got the SIGUSR1\n", getpid() );
+  printf( "Parent (pid %d) got the SIGUSR1 from child \n", getpid() );
 }
 
 int main()
@@ -18,21 +18,21 @@ int main()
   ret = fork();
   if (ret > 0)
   {       /* Parent Context */
-    printf( "Parent: This is the parent process (pid %d)\n",  getpid() );
+    printf("Parent: This is the parent process (pid %d) and my child has pid %d \n", getpid(), ret );
     signal( SIGUSR1, usr1_handler );  /* setting a signal handler */
     role = 0;
-    pause();
+    pause();   /* ready to receive signals */
     printf( "Parent: Awaiting child exit\n" );
     ret = wait( &status );
   }
   else if (ret == 0)
-  {
-    printf( " Child: This is the child process (pid %d)\n", getpid() );
+  {     /* Child context here */
+    printf(" Child: This is the child process (pid %d), my parent is pid %d \n", getpid(), getppid() );
     role = 1;
     sleep( 1 );
-    printf( " Child: Sending SIGUSR1 to pid %d\n", getppid() );
+    printf(" Child: Sending SIGUSR1 to pid %d\n", getppid() );
     kill( getppid(), SIGUSR1 );   /* sending signal to a specific process: the getppid() process */
-    sleep( 2 ); 
+    sleep( 2 );
   }
   else {   /* Parent Context — Error */
     printf( "Parent: Error trying to fork() (%d)\n", errno );
@@ -42,3 +42,4 @@ int main()
 }
 
 /* gcc -Wall page223_listing14.4_raise.c -o page223_listing14.4_raise */
+
